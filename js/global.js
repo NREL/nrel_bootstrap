205f6aa6530ebc6526c5cce343cb6d2ca5925b03
Shaun Laws <shaun.laws@nrel.gov>
2024-12-10 15:52:38 -0700

    Add support for BS4 accordions.

--- js/global.js
+++ js/global.js
@@ -9,7 +9,15 @@
 
   Drupal.behaviors.nrel_bootstrap = {
     attach: function (context, settings) {
-      $('[data-toggle="tooltip"]').tooltip();
+      $('[data-toggle="tooltip"]').each(function() {
+        var tooltip = new bootstrap.Tooltip($(this), options);
+      });
+      /* The Views Bootstrap accordions are missing the collapsed class when the page is first loaded. This is
+       * possibly a side-effect of using a template override to allow Views Bootstrap accordions to work with BS5 - @see
+       * https://www.drupal.org/project/views_bootstrap/issues/3168271 and
+       * templates/views_bootstrap/views-bootstrap-accordion.html.twig.
+       */
+      $('button.accordion-button').addClass('collapsed');
     }
   };
 
