a5e3b31384a8f70df455100bef0e02e0d3e3bd2e
Shaun Laws <shaun.laws@nrel.gov>
2024-12-19 11:35:25 -0700

    Update data-toggle and data-target to data-bs-toggle and data-bs-target.

--- js/global.js
+++ js/global.js
@@ -9,7 +9,7 @@
 
   Drupal.behaviors.nrel_bootstrap = {
     attach: function (context, settings) {
-      $('[data-toggle="tooltip"]').each(function() {
+      $('[data-bs-toggle="tooltip"]').each(function() {
         var tooltip = new bootstrap.Tooltip($(this), options);
       });
       /* The Views Bootstrap accordions are missing the collapsed class when the page is first loaded. This is
